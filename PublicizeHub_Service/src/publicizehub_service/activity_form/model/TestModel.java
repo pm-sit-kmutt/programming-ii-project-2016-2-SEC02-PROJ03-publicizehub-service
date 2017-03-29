f6c3ef7fbada466f78346bf1eb80f6d9e631fcc8
Jaja <Jaja@DESKTOP-4LOKC9F>
2017-03-30 00:51:25 +0700

    TestCommit - SubWork - Package

---
+++ PublicizeHub_Service/src/publicizehub_service/activity_form/model/TestModel.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package publicizehub_service.activity_form.model;
+
+/**
+ *
+ * @author Jaja
+ */
+public class TestModel {
+    
+}
